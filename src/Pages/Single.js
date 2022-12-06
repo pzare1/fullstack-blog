72d1d4ec180fafdcf75ba3d7d47179f34c02458e
Pouya <pouya@gmail.com>
2022-12-07 00:56:30 +0200

    changes in ui write page

--- src/Pages/Single.js
+++ src/Pages/Single.js
@@ -6,9 +6,35 @@ function Single() {
     return (
         <>
         <Nav/>
-        <div>
-            Single
-        </div>
+        <>
+            <div className="container"> 
+                <div className="row d-flex justify-content-between">
+                    <div className="col-md-8">
+                        <div className="img-holder">
+                        </div>
+                    </div>
+                    <div className="col-md-4 content-text">
+                        <h2 className="title-inside">How to made a good salad?</h2>
+                        <p className="des-inside">xuis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.</p>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="main-text p-4 m-3 ">
+                        {/* <p className="text-justify">xuis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.
+                        xuis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.
+                        xuis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.
+                        xuis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.
+                        xuis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.
+                        xuis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.
+                        xuis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.
+                        xuis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.
+                        xuis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.
+                        xuis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.                        
+                        </p> */}
+                    </div>
+                </div>
+            </div>
+        </>
         <Footer/>
         </>
     )
--- src/Pages/Write.js
+++ src/Pages/Write.js
@@ -1,13 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
 import Footer from '../Components/Footer'
 import Nav from '../Components/Nav'
 
 function Write() {
+    const [value, setValue] = useState('');
+    console.log(value);
     return (
         <>
-           <Nav/>
-           Write 
-           <Footer/>
+            <Nav />
+            <div className="container">
+                <div className="row mt-4">
+                    <div className="write mt-2 col-md-8">
+                        <label forHtml="text" className="mb-2">Title</label>
+                        <input type="text" className="form-control w-25" />
+                        <ReactQuill className="mt-4 h-50" theme="snow" value={value} onChange={setValue} />
+                    </div>
+                    <div className="col-md-4 custom-col">
+                        <div className="upload">
+                            <div className="d-flex justify-content-between">
+                                <input id="file-upload" type="file" />
+                                <button className="btn" style={{ backgroundColor: '#00b200', color: 'white' }}>Submit</button>
+                            </div>
+                        </div>
+                        <div className="category">
+                            <fieldset>
+                                <h5 className="mt-3">Select the category :</h5>
+
+                                <div>
+                                    <input type="radio" id="huey" name="drone" value="huey" checked />
+                                    <label forHtml="huey" className="ml-2">Food</label>
+                                </div>
+
+                                <div>
+                                    <input type="radio" id="dewey" name="drone" value="dewey" />
+                                    <label forHtml="dewey" className="ml-2">Work</label>
+                                </div>
+                                <div>
+                                    <input type="radio" id="louie" name="drone" value="louie" />
+                                    <label forHtml="louie" className="ml-2">Sport</label>
+                                </div>
+                            </fieldset>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <Footer />
         </>
     )
 }
